e9fb3504cd4373911c2d17930dcbffab882d4eef
CrisGadea <cristianhernangadea@gmail.com>
2023-09-05 20:32:11 -0300

    Clase Tamagotchi Test

--- src/main/java/tama/Tamagotchi.java
+++ src/main/java/tama/Tamagotchi.java
@@ -4,16 +4,44 @@ public class Tamagotchi {
 
     private Integer nivel;
 
+    private String estado;
+
+    // TODO: Refactor
+    private final Integer TIEMPO_EN_MINS = 90;
+
     public Tamagotchi(Integer nivel){
         this.nivel = nivel;
+        estado = "hambriento";
     }
 
     public Tamagotchi(){
         this.nivel = 0;
+        estado = "hambriento";
     }
 
     public Integer getNivel(){
         return nivel;
+    }
+
+    public void setEstado(String estado){
+        this.estado = estado;
+    }
+
+    public String getEstado(){
+        return estado;
+    }
+
+
+    // Metodos de la aplicacion
+
+    public void comer(){
+        if (estado.equals("hambriento")){
+            setEstado("contento");
+        } else if (estado.equals("contento")){
+            nivel++;
+        } else if (estado.equals("aburrido") && TIEMPO_EN_MINS > 80){
+            setEstado("contento");
+        }
     }
 
 }
--- src/test/java/tama/TamagotchiTest.java
+++ src/test/java/tama/TamagotchiTest.java
@@ -45,4 +45,41 @@ public class TamagotchiTest {
         Assert.assertEquals(nivelEsperado,nivelObtenido);
         Assert.assertEquals(nivelEsperado2,nivelObtenido2);
     }
+
+    @Test
+    public void testQuePruebaQuePuedoModificarElEstadoDeUnTama(){
+        Tamagotchi tama = new Tamagotchi();
+        String estadoEsperado = "aburrido";
+        tama.setEstado("aburrido");
+        Assert.assertEquals(estadoEsperado, tama.getEstado());
+    }
+
+    @Test
+    public void testQuePruebaQueAlComerElTamaHambrientoPasaAEstarContento(){
+        Tamagotchi tama = new Tamagotchi();
+        // En este caso, el tama de tener estado Hambriento
+        tama.setEstado("hambriento");
+        tama.comer();
+        String estadoEsperado = "contento";
+        Assert.assertEquals(estadoEsperado, tama.getEstado());
+    }
+
+    @Test
+    public void testQuePruebaQueAlComerEltamaContentoSuNivelSeIncrementaEnUno(){
+        Tamagotchi tama = new Tamagotchi(); // Su nivel inicial es 0
+        tama.setEstado("contento");
+        tama.comer();
+        Integer nivelEsperado = 1;
+        Assert.assertEquals(nivelEsperado, tama.getNivel());
+    }
+
+    @Test
+    public void testQuePruebaPasarDeAburridoAContentoSiSeAburreMasDe80Mins(){
+        Tamagotchi tama = new Tamagotchi();
+        // Pasaje a estado aburrido
+        tama.setEstado("aburrido");
+        String estadoEsperado = "contento";
+        tama.comer();
+        Assert.assertEquals(estadoEsperado, tama.getEstado());
+    }
 }
